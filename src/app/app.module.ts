2592a6221fee17a0f5a137d5f040ff7adea51ca0
Alaa-Hardan <hardanalaa212@gmail.com>
2024-08-19 15:16:04 +0300

    jj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { BranchComponent } from './branch/branch.component';
 import { ScrollComponent } from './scroll/scroll.component';
+import { StashComponent } from './stash/stash.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
     BranchComponent,
-    ScrollComponent
+    ScrollComponent,
+    StashComponent
   ],
   imports: [
     BrowserModule
