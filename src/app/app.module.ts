d06229bf20ce666ba24ad3421564ff7b962cf1e5
Alaa-Hardan <hardanalaa212@gmail.com>
2024-08-19 14:32:03 +0300

    added branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { BranchComponent } from './branch/branch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    BranchComponent
   ],
   imports: [
     BrowserModule
